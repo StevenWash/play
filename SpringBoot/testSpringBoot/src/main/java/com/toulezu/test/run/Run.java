5101490cfe1fcf4e1f6177f80a938aca4ad1e5a8
svsechen@163.com <svsechen@163.com>
2016-01-21 18:19:40 +0800

    增加定时任务功能 @EnableScheduling

--- SpringBoot/testSpringBoot/src/main/java/com/toulezu/test/run/Run.java
+++ SpringBoot/testSpringBoot/src/main/java/com/toulezu/test/run/Run.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"com.toulezu.test.springboot"})
+@ComponentScan(basePackages = {"com.toulezu.test"})
 @EnableAutoConfiguration
 public class Run {
 	public static void main(String[] args) {
